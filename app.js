9d7705a563cfd97dd8f0977237e9c2c200eef9f2
Gaston <gaston.irby@gmail.com>
2023-03-13 11:12:57 -0700

    lots of animation

--- app.js
+++ app.js
@@ -2,44 +2,46 @@ const title = document.querySelector('h1')
 const nav = document.querySelector('nav')
 const btn = document.querySelectorAll('a')
 
-// gsap.from(nav, {x: -700, duration: 4})
-// gsap.from(btn, {x: -200, duration: 2, })
+window.addEventListener('load', loader)
 
-
-gsap.from(nav, {
-    autoAlpha: 0,
-    y: -500,
-    ease: 'bounce',
-    duration: 3,
-    scrub: true
-    // stagger: {
-    //   each: 1,
-    //   from: 'center'
-    // }
+function loader () {
+  const TLLOAD = gsap.timeline({
+    default: {
+      ease: 'power2'
+    }
   })
 
-  window.addEventListener('load', loader);
-
-function loader() {
-    const TLLOAD = gsap.timeline({
-        default: {
-             ease: 'power2'
+  TLLOAD.to('.images-container', { height: 400, duration: 1.3, delay: 0.4 })
+    // .to('.bloc-txt', {height: 'auto', duration: 0.6}, '-=0.8')
+    // .to('.bloc-txt h2', {y: 0, duration: 0.6}, '-=0.6 ')
+    .to('.f2', { y: 0, duration: 0.6 })
+    .to('.flip-img1', { display: 'none', duration: 0 })
+    .to('.f2', { y: '-100%', duration: 0.6 })
+    .to('.load-container', { autoAlpha: 0, duration: 0.8, delay: 1 })
+    .from(nav, { autoAlpha: 0, y: -500, duration: 5, ease: 'power4', 
+    }, '-=1')
+    .from('.aboutme', {
+      duration: 1.5,
+      autoAlpha: 0,
+      y: 200,
+      scale: 1,
+      scrollTrigger: {
+        trigger: '.aboutme',
+        start: 'top 75%',
+        end: 'bottom 70%',
+        scrub: 5
+      }
+    })
+    .from('.projects', {
+        duration: 1.5,
+        autoAlpha: 0,
+        y: 200,
+        scale: 1,
+        scrollTrigger: {
+          trigger: '.projects',
+          start: 'top 90%',
+          end: 'bottom 80%',
+          scrub: 5
         }
     })
-
-    TLLOAD
-    .to('.images-container', {height: 400, duration: 1.3, delay: 0.4})
-    .to('.bloc-txt', {height: 'auto', duration: 0.6}, '-=0.8')
-    .to('.bloc-txt h2', {y: 0, duration: 0.6}, '-=0.6 ')
-    .to('.f2', {y: 0, duration: 0.6})
-    .to('.flip-img1', {display: 'none', duration: 0})
-    .to('.f2', {y: '-100%'})
-    .to('.load-container', {autoAlpha: 0, duration: 0.8, delay: 0.7})
-    .add(() => {
-        document.querySelector('video').play()
-    }, '-=0.8')
-    .add(() => {
-        document.querySelector('.load-container').getElementsByClassName.display = 'none'
-    })
-
 }
